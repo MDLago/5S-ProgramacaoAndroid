d882fe7ef5f4d553de0e850441ddf726571aef31
Marco Dionatan Lago <30815141+marcolago01@users.noreply.github.com>
2020-05-03 23:22:08 -0300

    ...

--- Calculadora/app/src/main/java/br/univali/prog/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/br/univali/prog/calculadora/MainActivity.java
@@ -25,31 +25,103 @@ public class MainActivity extends AppCompatActivity {
         resultado = findViewById(R.id.tvResultado);
     }
 
+
+    private String getNumString(int campo){
+        switch(campo){
+            case 1:
+                return numero1.getText().toString().trim();
+            case 2:
+                return numero2.getText().toString().trim();
+            default:
+                return null;
+        }
+    }
+
+    private boolean checkStringIsEmpty(String string){
+        return string.isEmpty();
+    }
+
+    private float convertStringToFloat(String string){
+        return Float.valueOf(string);
+    }
+
+    private void showResult(Float res){
+        resultado.setText("Resultado: " + res);
+    }
+
+    private void exibirAlertaLongo(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
     public void adicaoResultado (View v) {
         resultado.setText("");
 
-        String num1String = numero1.getText().toString().trim();
-        String num2String = numero2.getText().toString().trim();
+        String num1String = getNumString(1);
+        String num2String = getNumString(2);
 
-        if (num1String.equals("") || num2String.equals("")){
-            Toast.makeText(this, "Por favor, informar os dois números", Toast.LENGTH_LONG).show();
-        } else {
-            float res = Float.valueOf(num1String) + Float.valueOf(num2String);
-            resultado.setText("Resultado: " + res);
+        if(checkStringIsEmpty(num1String) || checkStringIsEmpty(num2String)){
+            exibirAlertaLongo(getResources().getString(R.string.Campo_numero_vazio));
+            return;
         }
+
+        float num1 = convertStringToFloat(num1String);
+        float num2 = convertStringToFloat(num2String);
+
+        float res = num1 + num2;
+        showResult(res);
     }
 
     public void subtracaoResultado (View v) {
         resultado.setText("");
 
-        String num1String = numero1.getText().toString().trim();
-        String num2String = numero2.getText().toString().trim();
+        String num1String = getNumString(1);
+        String num2String = getNumString(2);
 
-        if (num1String.equals("") || num2String.equals("")){
-            Toast.makeText(this, "Por favor, informar os dois números", Toast.LENGTH_LONG).show();
-        } else {
-            float res = Float.valueOf(num1String) - Float.valueOf(num2String);
-            resultado.setText("Resultado: " + res);
+        if(checkStringIsEmpty(num1String) || checkStringIsEmpty(num2String)){
+            exibirAlertaLongo(getResources().getString(R.string.Campo_numero_vazio));
+            return;
         }
+
+        float num1 = convertStringToFloat(num1String);
+        float num2 = convertStringToFloat(num2String);
+
+        float res = num1 - num2;
+        showResult(res);
+    }
+
+    public void subtracaoResultado (View v) {
+        resultado.setText("");
+
+        String num1String = getNumString(1);
+        String num2String = getNumString(2);
+
+        if(checkStringIsEmpty(num1String) || checkStringIsEmpty(num2String)){
+            exibirAlertaLongo(getResources().getString(R.string.Campo_numero_vazio));
+            return;
+        }
+
+        float num1 = convertStringToFloat(num1String);
+        float num2 = convertStringToFloat(num2String);
+
+        float res = num1 * num2;
+        showResult(res);
+    }
+
+    public void subtracaoResultado (View v) {
+        resultado.setText("");
+
+        String num1String = getNumString(1);
+        String num2String = getNumString(2);
+
+        if(checkStringIsEmpty(num1String) || checkStringIsEmpty(num2String)){
+            exibirAlertaLongo(getResources().getString(R.string.Campo_numero_vazio));
+            return;
+        }
+
+        float num1 = convertStringToFloat(num1String);
+        float num2 = convertStringToFloat(num2String);
+
+        float res = num1 / num2;
+        showResult(res);
     }
 }
