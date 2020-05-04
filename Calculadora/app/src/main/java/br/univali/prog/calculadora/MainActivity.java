d34bbd06120f3d4a880191542517698d9d2532f8
Marco Dionatan Lago <30815141+marcolago01@users.noreply.github.com>
2020-05-04 01:26:10 -0300

    atualização calculadora

--- Calculadora/app/src/main/java/br/univali/prog/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/br/univali/prog/calculadora/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText numero1;
     private EditText numero2;
+    private ArrayList<String> resultados;
 
-    private TextView resultado;
+    private ListView lvResultado;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +27,20 @@ public class MainActivity extends AppCompatActivity {
 
         numero1 = findViewById(R.id.etNum1);
         numero2 = findViewById(R.id.etNum2);
-        resultado = findViewById(R.id.tvResultado);
+        lvResultado = findViewById(R.id.lvResultado);
+
+        resultados = new ArrayList<String>();
+        adapter = historicoOperacoes(resultados);
+        lvResultado.setAdapter(adapter);
     }
 
+    public enum Operacao{
+        ADICAO,SUBTRACAO,DIVISAO,MULTIPLICACAO;
+    }
+
+    private ArrayAdapter<String> historicoOperacoes(ArrayList resultados){
+        return new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, resultados);
+    }
 
     private String getNumString(int campo){
         switch(campo){
@@ -45,8 +61,28 @@ public class MainActivity extends AppCompatActivity {
         return Float.valueOf(string);
     }
 
-    private void showResult(Float res){
-        resultado.setText("Resultado: " + res);
+    private void addResult(float num1, float num2, Operacao op, Float res){
+        switch (op){
+            case ADICAO:
+                resultados.add(num1 + " + " + num2 + " = " + res);
+                break;
+            case SUBTRACAO:
+                resultados.add(num1 + " - " + num2 + " = " + res);
+                break;
+            case MULTIPLICACAO:
+                resultados.add(num1 + " * " + num2 + " = " + res);
+                break;
+            case DIVISAO:
+                resultados.add(num1 + " / " + num2 + " = " + res);
+                break;
+            default:
+        }
+        exibirAlertaLongo("Teste");
+        updateResult();
+    }
+
+    private void updateResult(){
+        adapter.notifyDataSetChanged();
     }
 
     private void exibirAlertaLongo(String message){
@@ -54,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void adicaoResultado (View v) {
-        resultado.setText("");
 
         String num1String = getNumString(1);
         String num2String = getNumString(2);
@@ -68,11 +103,10 @@ public class MainActivity extends AppCompatActivity {
         float num2 = convertStringToFloat(num2String);
 
         float res = num1 + num2;
-        showResult(res);
+        addResult(num1,num2,Operacao.ADICAO,res);
     }
 
     public void subtracaoResultado (View v) {
-        resultado.setText("");
 
         String num1String = getNumString(1);
         String num2String = getNumString(2);
@@ -86,11 +120,10 @@ public class MainActivity extends AppCompatActivity {
         float num2 = convertStringToFloat(num2String);
 
         float res = num1 - num2;
-        showResult(res);
+        addResult(num1,num2,Operacao.SUBTRACAO,res);
     }
 
     public void multiplicacaoResultado (View v) {
-        resultado.setText("");
 
         String num1String = getNumString(1);
         String num2String = getNumString(2);
@@ -104,11 +137,10 @@ public class MainActivity extends AppCompatActivity {
         float num2 = convertStringToFloat(num2String);
 
         float res = num1 * num2;
-        showResult(res);
+        addResult(num1,num2,Operacao.MULTIPLICACAO,res);
     }
 
     public void divisaoResultado (View v) {
-        resultado.setText("");
 
         String num1String = getNumString(1);
         String num2String = getNumString(2);
@@ -121,7 +153,12 @@ public class MainActivity extends AppCompatActivity {
         float num1 = convertStringToFloat(num1String);
         float num2 = convertStringToFloat(num2String);
 
+        if (num2 == 0){
+            exibirAlertaLongo("Divisão por zero");
+            return;
+        }
+
         float res = num1 / num2;
-        showResult(res);
+        addResult(num1,num2,Operacao.DIVISAO,res);
     }
 }
